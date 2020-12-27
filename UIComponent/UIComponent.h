d0d76e074e22f9a84b211aeefe7deee3ef02afcf
原ひかる <harahikaru@harahikarunoMacBook-Pro.local>
2020-12-27 15:25:50 +0900

    UIComponent追加

---
+++ UIComponent/UIComponent.h
@@ -0,0 +1,18 @@
+//
+//  UIComponent.h
+//  UIComponent
+//
+//  Created by 原ひかる on 2020/12/27.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIComponent.
+FOUNDATION_EXPORT double UIComponentVersionNumber;
+
+//! Project version string for UIComponent.
+FOUNDATION_EXPORT const unsigned char UIComponentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIComponent/PublicHeader.h>
+
+
