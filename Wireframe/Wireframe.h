bfe28dca23be5fe3dd2182fa122ae52c4f79de68
hara-94 <hikaru.h9894@gmail.com>
2021-01-01 22:03:20 +0900

    target追加

---
+++ Wireframe/Wireframe.h
@@ -0,0 +1,18 @@
+//
+//  Wireframe.h
+//  Wireframe
+//
+//  Created by hikaruhara on 2021/01/01.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Wireframe.
+FOUNDATION_EXPORT double WireframeVersionNumber;
+
+//! Project version string for Wireframe.
+FOUNDATION_EXPORT const unsigned char WireframeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Wireframe/PublicHeader.h>
+
+
